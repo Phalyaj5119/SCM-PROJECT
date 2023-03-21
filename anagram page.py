3d82d96f2e63f9d5969f753ec30cadd92e365b09
parularora1703 <parularora1703@gmail.com>
2023-03-21 11:03:30 +0530

    MEDIUM LEVEL ADDED

--- anagram page.py
+++ anagram page.py
@@ -211,10 +211,254 @@ elif cho=='EASY':
     sum7 = sum5 + sum6 + sum4
 
     print("                               FINAL SCORE FOR PLAYER1: %d" % sum7)
-elif cho=='medium': 
-    print("yes2")
+elif cho=='medium':
+    l1 = ['bam', 'ban', 'ben', 'man', 'nab', 'nam', 'neb', 'nem', 'bae', 'bee', 'eme', 'mae', 'mea', 'ane', 'nae',
+          'nee', 'BAM', 'BAN', 'BEN', 'MAN', 'MEN', 'NAB', 'NAM', 'NEB', 'BAE', 'BEE', 'EME', 'MAE', 'MEA', 'ANE',
+          'NAE', 'NEE']
+    l2 = ['beam', 'bema', 'mabe', 'amen', 'bane', 'bean', 'been', 'bene', 'mane', 'mean', 'nabe', 'name', 'neem',
+          'nema', 'BEAM', 'BEMA', 'MABE', 'AMEN', 'BANE', 'BEAN', 'BEEN', 'BENE', 'MANE', 'MEAN', 'NABE', 'NAME',
+          'NEEM', 'NEMA']
+    l3 = ['maneb', 'enema', 'MANED', 'ENEMA']
+    l4 = ['BEMEAN', 'BENAME', 'bename', 'bemean']
+    print("                                              PLAYER 1")
+    print("                                             B/N/M/A/E/E")
+    sum1 = 0
+    print("THREE LETTER WORDS ONLY!")
+    for i in range(3):
+        val1 = str(input("enter ur word: "))
+        if val1 in l1:
+            sum1 += 100
+            print("your score is: %d" % sum1)
+            l1.remove(val1)
+        else:
+            print("INVALID")
+    print(" ")
+    print("FINAL SCORE:%d" % sum1)
+    print("")
+    print("                                             B/N/M/A/E/E")
+    print("FOUR LETTER WORDS ONLY!")
+    sum2 = 0
+    for i in range(3):
+        val2 = str(input("enter ur word: "))
+        if val2 in l2:
+            sum2 += 200
+            print("your score is: %d" % sum2)
+            l2.remove(val2)
+        else:
+            print("INVALID")
+    print(" ")
+    print("FINAL SCORE:%d" % sum2)
+    print("")
+    print("                                             B/N/M/A/E/E")
+    print("FIVE LETTER WORDS ONLY!")
+    sum3 = 0
+    for i in range(1):
+        val3 = str(input("enter ur word: "))
+        if val3 in l3:
+            sum3 += 400
+            print("your score is: %d" % sum3)
+            l3.remove(val3)
+        else:
+            print("INVALID")
+    print(" ")
+    print("FINAL SCORE:%d" % sum3)
+    print("")
+    print("SIX LETTER WORDS ONLY!")
+    sum4 = 0
+    for i in range(1):
+        val4 = str(input("enter ur word: "))
+        if val4 in l4:
+            sum4 += 1200
+            print("your score is: %d" % sum4)
+            l4.remove(val4)
+        else:
+            print("INVALID")
+    print(" ")
+    print("FINAL SCORE:%d" % sum4)
+    sum = sum3 + sum1 + sum2 + sum4
+
+    print("                               FINAL SCORE FOR PLAYER1: %d" % sum)
+    print(" ")
+    print("                                              PLAYER 2")
+    print("                                             B/N/M/A/E/E")
+    sum5 = 0
+    print("THREE LETTER WORDS ONLY!")
+    for i in range(3):
+        val5 = str(input("enter ur word: "))
+        if val5 in l1:
+            sum5 += 100
+            print("your score is: %d" % sum5)
+            l1.remove(val5)
+        else:
+            print("INVALID OR ALREADY USED BY PLAYER 1")
+    print(" ")
+    print("FINAL SCORE:%d" % sum5)
+    print("")
+    print("                                             B/N/M/A/E/E")
+    print("FOUR LETTER WORDS ONLY!")
+    sum6 = 0
+    for i in range(3):
+        val6 = str(input("enter ur word: "))
+        if val6 in l2:
+            sum6 += 200
+            print("your score is: %d" % sum6)
+            l2.remove(val6)
+        else:
+            print("INVALID OR ALREADY USED BY PLAYER 1")
+    print(" ")
+    print("FINAL SCORE:%d" % sum6)
+    print("")
+    print("                                             B/N/M/A/E/E")
+    print("FIVE LETTER WORDS ONLY!")
+    sum7 = 0
+    for i in range(1):
+        val7 = str(input("enter ur word: "))
+        if val7 in l3:
+            sum7 += 400
+            print("your score is: %d" % sum7)
+            l3.remove(val7)
+        else:
+            print("INVALID OR ALREADY USED BY PLAYER 1")
+    print(" ")
+    print("FINAL SCORE:%d" % sum7)
+    print("")
+    sum8 = 0
+    for i in range(1):
+        val8 = str(input("enter ur word: "))
+        if val8 in l4:
+            sum8 += 1200
+            print("your score is: %d" % sum8)
+        else:
+            print("INVALID OR ALREADY USED BY PLAYER 1")
+    print(" ")
+    print("FINAL SCORE:%d" % sum8)
+    sum9 = sum8 + sum6 + sum5 + sum7
+
+    print("                               FINAL SCORE FOR PLAYER1: %d" % sum9)
 elif cho=='MEDIUM':
-    print("yes3")
+    l1 = ['bam', 'ban', 'ben', 'man', 'nab', 'nam', 'neb', 'nem', 'bae', 'bee', 'eme', 'mae', 'mea', 'ane', 'nae',
+          'nee', 'BAM', 'BAN', 'BEN', 'MAN', 'MEN', 'NAB', 'NAM', 'NEB', 'BAE', 'BEE', 'EME', 'MAE', 'MEA', 'ANE',
+          'NAE', 'NEE']
+    l2 = ['beam', 'bema', 'mabe', 'amen', 'bane', 'bean', 'been', 'bene', 'mane', 'mean', 'nabe', 'name', 'neem',
+          'nema', 'BEAM', 'BEMA', 'MABE', 'AMEN', 'BANE', 'BEAN', 'BEEN', 'BENE', 'MANE', 'MEAN', 'NABE', 'NAME',
+          'NEEM', 'NEMA']
+    l3 = ['maneb', 'enema', 'MANED', 'ENEMA']
+    l4 = ['BEMEAN', 'BENAME', 'bename', 'bemean']
+    print("                                              PLAYER 1")
+    print("                                             B/N/M/A/E/E")
+    sum1 = 0
+    print("THREE LETTER WORDS ONLY!")
+    for i in range(3):
+        val1 = str(input("enter ur word: "))
+        if val1 in l1:
+            sum1 += 100
+            print("your score is: %d" % sum1)
+            l1.remove(val1)
+        else:
+            print("INVALID")
+    print(" ")
+    print("FINAL SCORE:%d" % sum1)
+    print("")
+    print("                                             B/N/M/A/E/E")
+    print("FOUR LETTER WORDS ONLY!")
+    sum2 = 0
+    for i in range(3):
+        val2 = str(input("enter ur word: "))
+        if val2 in l2:
+            sum2 += 200
+            print("your score is: %d" % sum2)
+            l2.remove(val2)
+        else:
+            print("INVALID")
+    print(" ")
+    print("FINAL SCORE:%d" % sum2)
+    print("")
+    print("                                             B/N/M/A/E/E")
+    print("FIVE LETTER WORDS ONLY!")
+    sum3 = 0
+    for i in range(1):
+        val3 = str(input("enter ur word: "))
+        if val3 in l3:
+            sum3 += 400
+            print("your score is: %d" % sum3)
+            l3.remove(val3)
+        else:
+            print("INVALID")
+    print(" ")
+    print("FINAL SCORE:%d" % sum3)
+    print("")
+    print("SIX LETTER WORDS ONLY!")
+    sum4 = 0
+    for i in range(1):
+        val4 = str(input("enter ur word: "))
+        if val4 in l4:
+            sum4 += 1200
+            print("your score is: %d" % sum4)
+            l4.remove(val4)
+        else:
+            print("INVALID")
+    print(" ")
+    print("FINAL SCORE:%d" % sum4)
+    sum = sum3 + sum1 + sum2 + sum4
+
+    print("                               FINAL SCORE FOR PLAYER1: %d" % sum)
+    print(" ")
+    print("                                              PLAYER 2")
+    print("                                             B/N/M/A/E/E")
+    sum5 = 0
+    print("THREE LETTER WORDS ONLY!")
+    for i in range(3):
+        val5 = str(input("enter ur word: "))
+        if val5 in l1:
+            sum5 += 100
+            print("your score is: %d" % sum5)
+            l1.remove(val5)
+        else:
+            print("INVALID OR ALREADY USED BY PLAYER 1")
+    print(" ")
+    print("FINAL SCORE:%d" % sum5)
+    print("")
+    print("                                             B/N/M/A/E/E")
+    print("FOUR LETTER WORDS ONLY!")
+    sum6 = 0
+    for i in range(3):
+        val6 = str(input("enter ur word: "))
+        if val6 in l2:
+            sum6 += 200
+            print("your score is: %d" % sum6)
+            l2.remove(val6)
+        else:
+            print("INVALID OR ALREADY USED BY PLAYER 1")
+    print(" ")
+    print("FINAL SCORE:%d" % sum6)
+    print("")
+    print("                                             B/N/M/A/E/E")
+    print("FIVE LETTER WORDS ONLY!")
+    sum7 = 0
+    for i in range(1):
+        val7 = str(input("enter ur word: "))
+        if val7 in l3:
+            sum7 += 400
+            print("your score is: %d" % sum7)
+            l3.remove(val7)
+        else:
+            print("INVALID OR ALREADY USED BY PLAYER 1")
+    print(" ")
+    print("FINAL SCORE:%d" % sum7)
+    print("")
+    sum8 = 0
+    for i in range(1):
+        val8 = str(input("enter ur word: "))
+        if val8 in l4:
+            sum8 += 1200
+            print("your score is: %d" % sum8)
+        else:
+            print("INVALID OR ALREADY USED BY PLAYER 1")
+    print(" ")
+    print("FINAL SCORE:%d" % sum8)
+    sum9 = sum8 + sum6 + sum5 + sum7
+
+    print("                               FINAL SCORE FOR PLAYER1: %d" % sum9)
 elif cho=='HARD':
     l1 = ['amp', 'map', 'pam', 'ape', 'arm', 'eme', 'mae', 'mar', 'mea', 'mer', 'par', 'pea', 'pee', 'per', 'pre',
           'ram', 'rap', 'rem', 'rep', 'are', 'AMP', 'MAP', 'PAM', 'APE', 'ARM', 'EME', 'MAE', 'MER', 'PAR', 'PEA',
